bfda598517f2c45607c0c30f493013cb8bd2bde3
TheShady14 <benlombaard820@gmail.com>
2026-02-22 18:15:41 +0000

    Configure static export for Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
   typescript: {
     ignoreBuildErrors: true,
-  }
-}
+  },
+};
 
-export default nextConfig
+export default nextConfig;
