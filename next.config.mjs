cf3c2a11d340a1cd72ef7a335d590f0dc2a415e4
TheShady14 <benlombaard820@gmail.com>
2026-02-22 18:29:18 +0000

    Final static export setup for Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  images: {
-    unoptimized: true,
-  },
-
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
